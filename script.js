3b6c9f906f0476b9df8e9b67f45188c485e063a0
Tania <tania.dev77@gmail.com>
2015-06-21 00:16:59 -0700

    add in saerch sort and md5 email hash

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
 // Code goes here
 
+var app = angular.module('contacts', ['angular-md5']);
+app.controller('ContactsCtrl', function($scope, md5){
+  console.log("ctrl");
+  $scope.contacts = [
+    {fname:"tania",
+    lname:"leon",
+    email:"valentina531@gmail.com",
+    phone:"444 3333",
+    grav:"grav"},
+
+    {fname:"stan",
+    lname:"liu",
+    email:"skl@gmail.com",
+    phone:"444 3432",
+    gravatar:"s ta"},
+
+    {fname:"bonne",
+    lname:"so",
+    email:"so_bonnie@gmail.com",
+    phone:"444 3432",
+    gravatar:"b ta"}
+  ];
+  $scope.contact = {};
+  $scope.addContact = function(contact){
+    console.log("click");
+    $scope.contacts.push(contact);
+    console.log("$scope.contacts: ", $scope.contacts);
+    $scope.contact = '';
+  };
+  $scope.$watch('email' ,function() {
+    $scope.message = 'Your email Hash is: ' + md5.createHash($scope.email || '');
+  });
+
+});
